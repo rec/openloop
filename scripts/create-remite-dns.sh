9477f4381af0a2f035d3283db42f7ec6b0733885
Tom Ritchford <tom@swirly.com>
2026-09-26 16:10:52 +0200

    fixup! Add DNS script

--- scripts/create-remite-dns.sh
+++ scripts/create-remite-dns.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cloudflare_account_id=44066e01f0c0660a57222615b9fc72e1
+
 read -rsp 'Cloudflare API token: ' cf_api_token
 echo
 
 zone_id=$(
   curl -fsS \
     -H "Authorization: Bearer $cf_api_token" \
-    'https://api.cloudflare.com/client/v4/zones?name=ax.to&status=active' |
+    "https://api.cloudflare.com/client/v4/zones?account.id=$cloudflare_account_id&name=ax.to&status=active" |
     jq -er '.result[0].id'
 )
 server_ip=$(dig +short A server.swirly.com | head -n 1)
